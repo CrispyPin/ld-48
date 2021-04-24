7665b4e33aa33bc40dbd1dd756269ba85371e9b6
CrispyPin <crispin@tasa.se>
2021-04-24 11:22:38 +0200

    add camera zoom

--- ld-48/scripts/player.gd
+++ ld-48/scripts/player.gd
@@ -2,6 +2,9 @@ extends Spatial
 
 export var sensitivity_h = 1.0
 export var sensitivity_v = 1.0
+export var zoom_factor = 1.5
+export var zoom_min = 5
+export var zoom_max = 50
 
 
 func _ready():
@@ -15,3 +18,10 @@ func _input(event):
 		angle_v = max(PI *-0.5 - $CameraRoot.rotation.x, angle_v)
 		$CameraRoot.rotate_object_local(Vector3(1,0,0), angle_v)
 
+func _process(_delta):
+	if Input.is_action_just_released("zoom_in"):
+		if -$CameraRoot/Camera.transform.origin.z > zoom_min:
+			$CameraRoot/Camera.transform.origin /= zoom_factor
+	elif Input.is_action_just_released("zoom_out"):
+		if -$CameraRoot/Camera.transform.origin.z < zoom_max:
+			$CameraRoot/Camera.transform.origin *= zoom_factor
