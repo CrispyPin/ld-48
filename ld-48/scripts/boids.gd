eb7ecf0f9f84345eac7959b37c5da32da464f380
CrispyPin <crispin@tasa.se>
2021-04-24 17:59:06 +0200

    fix indentation issue

--- ld-48/scripts/boids.gd
+++ ld-48/scripts/boids.gd
@@ -22,19 +22,19 @@ export (float, 0.0, 16.0) var radiusAttract=8
 export (float, 0.0, 32.0) var radiusPlayer=16
 
 func randVec(l=1):
-	return Vector3(rand_range(-l,l), rand_range(-l,l), rand_range(-l,l))
+    return Vector3(rand_range(-l,l), rand_range(-l,l), rand_range(-l,l))
 
 
 func randVecNoZ(l=1):
-	return Vector3(rand_range(-l,l), rand_range(-l,l), 0)
+    return Vector3(rand_range(-l,l), rand_range(-l,l), 0)
 
 func addBoid(position=randVec(20), type=0, rotation=randVecNoZ(PI)):
-	var boid = boidResource.instance()
-	boid.translation = position 
-	boid.rotation = rotation
-	boid.type = type
-	boidList.append(boid) 
-	add_child(boid)
+    var boid = boidResource.instance()
+    boid.translation = position
+    boid.rotation = rotation
+    boid.type = type
+    boidList.append(boid)
+    add_child(boid)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -42,63 +42,63 @@ func _ready():
     semaphore = Semaphore.new()
     thread=Thread.new()
     thread.start(self, "_updateBoidThreadFunc")
-    
-	randomize()
-	boidResource = load(boidResourcePath)
+
+    randomize()
+    boidResource = load(boidResourcePath)
     for i in range(0,100):
-	    addBoid()
+        addBoid()
 
 func getDir(node):
-	return node.transform.basis.z
+    return node.transform.basis.z
 
 func dirToEuclid(vec):
-	var x = vec.angle_to(Vector3(0,1,0))
-	var y = Vector2(vec.x,vec.y).angle()
-	var z = 3
-	return Vector3(x,y,z)
+    var x = vec.angle_to(Vector3(0,1,0))
+    var y = Vector2(vec.x,vec.y).angle()
+    var z = 3
+    return Vector3(x,y,z)
 
 func dirToRotX(vec):
-	return vec.angle_to(Vector3(0,1,0))
+    return vec.angle_to(Vector3(0,1,0))
 
-#update data in boid 
+#update data in boid
 func updateBoid(boid, other, delta):
-	#if canSee(boid, other):
-	if true:
-		var numBoids = len(boidList)
-		var pdiff = (boid.translation - other.translation)
-		var dist = pdiff.length()
-		var steerTarget = Vector3(0,0,0)
-		
-		var distFactor = 1/dist
+    #if canSee(boid, other):
+    if true:
+        var numBoids = len(boidList)
+        var pdiff = (boid.translation - other.translation)
+        var dist = pdiff.length()
+        var steerTarget = Vector3(0,0,0)
+
+        var distFactor = 1/dist
 
 
-		#prevent collision with other boids
-		if dist<radiusCollide:
-			steerTarget += 5*(distFactor/dist/dist-(1/radiusCollide/radiusCollide))*pdiff.normalized()*collidePreventStrength
+        #prevent collision with other boids
+        if dist<radiusCollide:
+            steerTarget += 5*(distFactor/dist/dist-(1/radiusCollide/radiusCollide))*pdiff.normalized()*collidePreventStrength
 
-		#steer towards nearby boid
-		elif dist<radiusAttract:
-			steerTarget -= (cos(
-				(dist-radiusCollide)*2*PI/(radiusAttract-radiusCollide)     
-			)-1)*pdiff.normalized()*nearbySteerStrength
+        #steer towards nearby boid
+        elif dist<radiusAttract:
+            steerTarget -= (cos(
+                (dist-radiusCollide)*2*PI/(radiusAttract-radiusCollide)
+            )-1)*pdiff.normalized()*nearbySteerStrength
 
 
-		#make steering equal other boid
-		if dist<radiusAttract:
-			steerTarget += getDir(other)*copyDirStrength
+        #make steering equal other boid
+        if dist<radiusAttract:
+            steerTarget += getDir(other)*copyDirStrength
 
-		boid.steerTarget+=steerTarget
+        boid.steerTarget+=steerTarget
 
 #180 degree FOV
 func canSee(boid, other):
-	return (boid.translation - other.translation).dot(getDir(boid))<0
+    return (boid.translation - other.translation).dot(getDir(boid))<0
 
 #move boid
 func moveBoid(boid, delta):
-	boid.translation+=getDir(boid)*delta*5
-	#boid.look_at(boid.steerTarget + boid.translation, Vector3(0,1,0))
-	#boid.look_at(-boid.steerTarget + boid.translation, Vector3(0,1,0))
-	boid.transform = boid.transform.looking_at(-boid.steerTarget + boid.translation, Vector3(0,1,0))
+    boid.translation+=getDir(boid)*delta*5
+    #boid.look_at(boid.steerTarget + boid.translation, Vector3(0,1,0))
+    #boid.look_at(-boid.steerTarget + boid.translation, Vector3(0,1,0))
+    boid.transform = boid.transform.looking_at(-boid.steerTarget + boid.translation, Vector3(0,1,0))
 
 
 var imod = 0
@@ -114,8 +114,8 @@ func _process(delta):
 
         semaphore.post()
     mutex.lock()
-	for boid in boidList:
-		moveBoid(boid, delta)
+    for boid in boidList:
+        moveBoid(boid, delta)
     mutex.unlock()
     imod+=1
 
@@ -129,20 +129,20 @@ func updateBoids(delta):
 
     semaphore.wait()
 
-	for boid in boidList:
+    for boid in boidList:
         #mutex.lock()
-		for other in boidList:	
+        for other in boidList:
             pass
             if boid!=other:
                 updateBoid(boid, other, delta)
 
-		#move away from player
+        #move away from player
         var dp = (boid.translation-player.translation)
         var dist = dp.length()
         if dist<radiusPlayer:
-		    boid.steerTarget += dp*(1/dist/dist-1/radiusPlayer/radiusPlayer)*100*avoidPlayerStrength
+            boid.steerTarget += dp*(1/dist/dist-1/radiusPlayer/radiusPlayer)*100*avoidPlayerStrength
 
-		#move to center
-		boid.steerTarget += -boid.translation.normalized()*0.1*centerStrength
+        #move to center
+        boid.steerTarget += -boid.translation.normalized()*0.1*centerStrength
         #mutex.unlock()
 
