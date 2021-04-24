3f3c8f0bcd2ae65918a94a87c294dabccb40364d
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-04-24 12:44:24 +0200

    boid FOV

--- ld-48/scripts/boids.gd
+++ ld-48/scripts/boids.gd
@@ -10,64 +10,52 @@ var boidResource
 func randVec(l=1):
     return Vector3(rand_range(-l,l), rand_range(-l,l), rand_range(-l,l))
 
-func addBoid(position=randVec(5), type=0, rotation=randVec(PI)):
 
+func randVecNoZ(l=1):
+    return Vector3(rand_range(-l,l), rand_range(-l,l), 0)
 
-	var boid = boidResource.instance()
-	boid.translation = position 
+func addBoid(position=randVec(20), type=0, rotation=randVecNoZ(PI)):
+    var boid = boidResource.instance()
+    boid.translation = position 
     boid.rotation = rotation
     boid.type = type
-	boidList.append(boid) 
-	add_child(boid)
+    boidList.append(boid) 
+    add_child(boid)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     randomize()
-	boidResource = load(boidResourcePath)
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
-	addBoid()
+    boidResource = load(boidResourcePath)
+    addBoid()
+    addBoid()
+    addBoid()
+    addBoid()
+    addBoid()
+    addBoid()
+    addBoid()
 
 func getDir(node):
     return node.transform.basis.z
 
+#update data in boid 
 func updateBoid(boid, other, delta):
-    var pdiff = boid.translation - other.translation
-    if pdiff.dot(getDir(boid))<0:
-        #move
-        boid.translation+=boid.transform.basis.z*delta
-	
+    if canSee(boid, other):
+        boid.type=1
 
+#180 degree FOV
+func canSee(boid, other):
+    return (boid.translation - other.translation).dot(getDir(boid))<0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#move boid
+func moveBoid(boid, delta):
+    if boid.type==1:
+        boid.translation+=getDir(boid)*delta
+
 func _process(delta):
     for boid in boidList:	
+        boid.type=0
         for other in boidList:	
             if boid!=other:
                 updateBoid(boid, other, delta)
+        moveBoid(boid, delta)
+
