eba9caeb18a675d1dbb9c7c19029ac38879170d2
ErikBot42 <erik.arn.magnusson@gmail.com>
2021-04-24 09:44:11 +0200

    haha here is a crash report

--- ld-48/scripts/boids.cs
+++ ld-48/scripts/boids.cs
@@ -3,11 +3,8 @@ using System;
 
 public class boids : Node
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
 
-	// Called when the node enters the scene tree for the first time.
+	export Node boidScene;
 	public override void _Ready()
 	{
 		
