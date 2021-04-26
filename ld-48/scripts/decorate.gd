e927dc622f949d70f770d294477d3d1ff6784e57
CrispyPin <crispin@tasa.se>
2021-04-26 15:05:36 +0200

    add flowers to cave branches

--- ld-48/scripts/decorate.gd
+++ ld-48/scripts/decorate.gd
@@ -4,7 +4,8 @@ export var self_activate = false
 
 var decor = [preload("res://models/seagrass/seagrass-1.tscn"),
 preload("res://models/seagrass/seagrass-2.tscn"),
-preload("res://models/seagrass/seagrass-3.tscn")]
+preload("res://models/seagrass/seagrass-3.tscn"),
+preload("res://models/seagrass/flower-1.tscn")]
 
 onready var cave_root = get_node("/root/Game/CaveRoot")
 onready var seg_scale = cave_root.seg_scale
@@ -21,9 +22,11 @@ func add_decor():
     for vi in range(mdt.get_vertex_count()):
         var vert = mdt.get_vertex(vi)
         if randf() > 0.8:
-            var type = randi() % len(decor)
+            var type = randi() % (len(decor)-1)
+            if vert.length() > 2.3:
+                type = 3
             var d = decor[type].instance()
             add_child(d)
             d.global_transform.origin = global_transform.xform(vert)
-            d.scale = Vector3(1,1,1) / 5.0
-            d.rotation = Vector3(randf(), randf(), randf())*3.14
+            d.scale = Vector3(1,1,1) / 6.0
+            d.rotation = Vector3(randf(), randf(), randf())*PI*2
