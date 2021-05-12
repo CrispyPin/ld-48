46d2ab2b2d4ee63a0f2103ec42fe2a0819c5c7b1
CrispyPin <crispin@tasa.se>
2021-05-12 22:47:08 +0200

    add ui from template, create settings

---
+++ ld-48/scripts/ui/global.gd
@@ -0,0 +1,77 @@
+extends Node
+
+
+const DEBUG_SETTINGS = false
+const SETTINGS_PATH = "user://settings.json"
+const SETTINGS_DEF = {
+    "fullscreen": {
+        "name": "Fullscreen",
+        "flags": [],
+        "tooltip": "fullscreen",
+        "type": "toggle",
+        "default": false
+    },
+    "boid_count": {
+        "name": "Fish amount: ",
+        "flags": ["main_menu_only"],
+        "tooltip": "Number of boids, high performance impact.",
+        "type": "choice",
+        "default": 1,
+        "options": ["256", "512", "1024", "2048"]
+    }
+}
+
+var game_loaded = false
+var settings_loaded = false
+var paused = false
+var settings = {}
+
+
+func _ready() -> void:
+    _init_settings()
+    load_settings()
+    settings_loaded = true
+    get_node("/root/Default").queue_free()
+
+
+func set_setting(key, val):
+    settings[key] = val
+    if DEBUG_SETTINGS:
+        print("Settings changed. ", key, ": ", val)
+    save_settings()
+
+
+func _init_settings():
+    for key in SETTINGS_DEF:
+        settings[key] = SETTINGS_DEF[key].default
+    if DEBUG_SETTINGS:
+        print("Default settings: ", settings)
+
+
+func save_settings():
+    var file = File.new()
+    file.open(SETTINGS_PATH, File.WRITE)
+    file.store_line(to_json(settings))
+    file.close()
+
+
+func load_settings() -> void:
+    var file = File.new()
+
+    if not file.file_exists(SETTINGS_PATH):
+        if DEBUG_SETTINGS:
+            print("No settings file exists, using defaults")
+        return
+
+    file.open(SETTINGS_PATH, File.READ)
+    var new_settings = parse_json(file.get_as_text())
+    file.close()
+
+    # in case settings format has changed, this is better than just copying
+    for key in new_settings:
+        if settings.has(key):
+            settings[key] = new_settings[key]
+    save_settings()
+    if DEBUG_SETTINGS:
+        print("Loaded settings from file")
+        print(settings)
---
+++ ld-48/scripts/ui/menu_game.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+export var pause_game = true
+
+
+func _on_ResumeButton_pressed() -> void:
+    UI.set_menu()
+
+func _on_SettingsButton_pressed() -> void:
+    UI.set_menu("Settings")
+
+func _on_ExitButton_pressed() -> void:
+    UI.stop_game()
